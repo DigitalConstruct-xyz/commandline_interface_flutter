e5bc1637ba47e6dce9370b624963d9958caa9537
monki1 <kxye111@gmail.com>
2022-12-26 20:33:57 -0500

    a convenient generator function for from CLIManger to CLIWidget

--- lib/commandline_interface.dart
+++ lib/commandline_interface.dart
@@ -1,4 +1,12 @@
+import 'commandline_interface.dart';
 export 'src/cli_widget.dart';
 export 'src/cli_controller.dart';
 export 'src/cli_manager.dart';
+
+
+CLIWidget cliWidgetGenerator(CLIManagerBase manager){
+  CLIController cliController = CLIController(initManager: manager);
+  CLIWidget cliWidget = CLIWidget(cliController);
+  return cliWidget;
+}
 
